9788b02876ed111e9da95e684d4efe1ce31f63e3
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-09-10 12:22:17 +0100

    refactor: ♻️  add `app-bullet-detail` class

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,15 @@ export default function Home() {
         <a href="https://discord.gg/8bSKaepXv8" className="bg-off-white py-2 px-4 rounded-[80px] text-white font-semibold hover:rounded-lg transition-all ease-in-out">Download</a>
 
         <div className="border-y-[1px] border-border flex flex-col md:gap-8 md:py-10 md:flex-row">
-          <div className="flex-1 text-center md:text-left py-8 md:py-0">
+          <div className="app-bullet-detail">
             <h2 className="text-lg font-semibold">Intuitive UI</h2>
             <h3 className="text-sm opacity-70">A sleek and straightforward method for streaming content from your preferred websites, Chouten employs a customized user interface that seamlessly integrates with native video playback features.</h3>
           </div>
-          <div className="flex-1 border-y-2 md:border-y-0 py-8 md:py-0 text-center md:text-left">
+          <div className="app-bullet-detail border-y-2 border-off-white md:border-y-0">
             <h2 className="text-lg font-semibold">Ad-free</h2>
             <h3 className="text-sm opacity-70">Delight in an ad-free application that offers seamless streaming, devoid of intrusive advertisements and tracking, promoting both enhanced speed and privacy.</h3>
           </div>
-          <div className="flex-1 text-center md:text-left py-8 md:py-0">
+          <div className="app-bullet-detail">
             <h2 className="text-lg font-semibold">Modularity</h2>
             <h3 className="text-sm opacity-70">Chouten operates as a versatile modular system, adept at retrieving and presenting information from an array of third-party listing websites. It seamlessly interfaces with tracking services, facilitating the exchange of data to enhance user experiences and functionality.</h3>
           </div>
